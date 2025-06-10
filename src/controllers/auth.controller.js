a5788faec3487d4adb18b6f5b6d540621c2ee41f
Muhammad <muhammmmadazizekubov@gmail.com>
2025-06-10 23:51:14 +0500

    delete all

--- src/controllers/auth.controller.js
+++
@@ -1,6 +0,0 @@
-import { userService } from '../services/auth.service.js';
-import { comparePassword } from '../utils/bcrypt.js';
-import { validations } from '../utils/validations.js';
-import { generateAccessToken, generateRefreshToken } from '../utils/generate-token.js';
-import logger from '../config/winston.js';
-
--- src/server.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js';
-import config from './config/index.js';
-import logger from './config/winston.js';
-
-const PORT = config.port;
-
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-});
